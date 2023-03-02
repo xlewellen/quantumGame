6406fa4ff22fe9e370b58afe54de2eb32950cb49
xlewellen <xalewellen@gmail.com>
2023-03-01 21:04:55 -0600

    Added reticle collision

---
+++ QuantumGame/Assets/Reticle.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Reset MoveDelta
+        moveDelta = Vector3.zero;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x, y, 0);
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Reticle", "Blocking"));
+        Debug.Log(hit);
+        if (hit.collider == null) {
+
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Reticle", "Blocking"));
+        if (hit.collider == null)
+        {
+
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+
+        }
+    }
+}
