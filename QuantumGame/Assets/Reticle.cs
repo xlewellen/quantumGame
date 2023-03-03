06d0b0352ace123981d6c54ba7730d04bd75b4a1
xlewellen <xalewellen@gmail.com>
2023-03-02 23:55:32 -0600

    grid lock movement

--- QuantumGame/Assets/Reticle.cs
+++ QuantumGame/Assets/Reticle.cs
@@ -8,6 +8,7 @@ public class Reticle : MonoBehaviour
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
     private RaycastHit2D hit;
+    private double counter;
 
     // Start is called before the first frame update
     private void Start()
@@ -15,9 +16,22 @@ public class Reticle : MonoBehaviour
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
+    private void MoveWait(double time) {
+        double counter = 0.0;
+        while (counter < time) {
+            counter += Time.deltaTime;
+        }
+        return;
+    }
+
     private void FixedUpdate()
     {
         //Reset MoveDelta
+
+        double moveTime = 0.2;
+        
+        if (counter < moveTime) counter += Time.deltaTime;
+
         moveDelta = Vector3.zero;
 
         float x = Input.GetAxisRaw("Horizontal");
@@ -25,20 +39,26 @@ public class Reticle : MonoBehaviour
 
         moveDelta = new Vector3(x, y, 0);
 
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Reticle", "Blocking"));
-        Debug.Log(hit);
-        if (hit.collider == null) {
+/*        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Reticle", "Blocking"));
+        Debug.Log(hit);*/
+        if (counter >= moveTime) {
 
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+            transform.Translate(Mathf.Ceil(moveDelta.x), Mathf.Ceil(moveDelta.y), 0);
+            counter = 0;
 
         }
 
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Reticle", "Blocking"));
-        if (hit.collider == null)
+/*        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Reticle", "Blocking"));
+        if (hit.collider == null && counter >= moveTime)
         {
 
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+            transform.Translate(Mathf.Ceil(moveDelta.x), 0, 0);
+            counter = 0;
 
-        }
+        }*/
+
+        var currentPos = transform.position;
+        transform.position = new Vector3(Mathf.Round(currentPos.x), Mathf.Round(currentPos.y), Mathf.Round(currentPos.z));
+
     }
 }
