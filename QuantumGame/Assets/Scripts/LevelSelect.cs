6aa000e3f34a21e3c794db138edd20346144b2a9
pipietrzyk <pipietrzyk0@gmail.com>
2023-03-04 18:37:27 -0600

    More levels

--- QuantumGame/Assets/Scripts/LevelSelect.cs
+++ QuantumGame/Assets/Scripts/LevelSelect.cs
@@ -7,6 +7,9 @@ public class LevelSelect : MonoBehaviour
 {
 
     public string menuScene;
+    public string lvl1, lvl2,  lvl3, lvl4, lvl5;
+
+    private int curLevel = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +24,33 @@ public class LevelSelect : MonoBehaviour
     }
 
     public void BackToMenu() {
+        SceneManager.LoadScene(menuScene);
+    }
+
+    // TODO: change menuScene with the approproate lvl variable
+
+    public void LevelOne() {
+        curLevel = 1;
+        SceneManager.LoadScene(menuScene);
+    }
+
+    public void LevelTwo() {
+        curLevel = 2;
+        SceneManager.LoadScene(menuScene);
+    }
+
+    public void LevelThree() {
+        curLevel = 3;
+        SceneManager.LoadScene(menuScene);
+    }
+
+    public void LevelFour() {
+        curLevel = 4;
+        SceneManager.LoadScene(menuScene);
+    }
+
+    public void LevelFive() {
+        curLevel = 5;
         SceneManager.LoadScene(menuScene);
     }
 }
