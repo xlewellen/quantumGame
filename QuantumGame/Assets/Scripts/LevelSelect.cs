701445ae6ea01d25ac6ee226c4750040cb7aa221
pipietrzyk <pipietrzyk0@gmail.com>
2023-03-04 19:12:46 -0600

    Filling inventory based on level selected

--- QuantumGame/Assets/Scripts/LevelSelect.cs
+++ QuantumGame/Assets/Scripts/LevelSelect.cs
@@ -6,8 +6,15 @@ using UnityEngine.SceneManagement;
 public class LevelSelect : MonoBehaviour
 {
 
+    int TRACKS = 0;
+    int H_GATE = 1;
+    int Z_GATE = 2;
+    int NOT_GATE = 3;
+    int M_GATE = 4;
+    int[] inventory = new int[5];
+
     public string menuScene;
-    public string lvl1, lvl2,  lvl3, lvl4, lvl5;
+    public string lvl1, lvl2, lvl3, lvl4, lvl5;
 
     private int curLevel = 1;
 
@@ -30,27 +37,43 @@ public class LevelSelect : MonoBehaviour
     // TODO: change menuScene with the approproate lvl variable
 
     public void LevelOne() {
-        curLevel = 1;
+        makeInv(curLevel = 1);
         SceneManager.LoadScene(menuScene);
     }
 
     public void LevelTwo() {
-        curLevel = 2;
+        makeInv(curLevel = 2);
         SceneManager.LoadScene(menuScene);
     }
 
     public void LevelThree() {
-        curLevel = 3;
+        makeInv(curLevel = 3);
         SceneManager.LoadScene(menuScene);
     }
 
     public void LevelFour() {
-        curLevel = 4;
+        makeInv(curLevel = 4);
         SceneManager.LoadScene(menuScene);
     }
 
     public void LevelFive() {
-        curLevel = 5;
+        makeInv(curLevel = 5);
         SceneManager.LoadScene(menuScene);
     }
+
+// TODO: Add cases here to fill the inventory for each level
+    private void makeInv(int lvl) {
+        
+        switch (lvl) {
+
+            case 1:
+                inventory[TRACKS] = 1;
+                inventory[H_GATE] = 2;
+                inventory[Z_GATE] = 3;
+                inventory[NOT_GATE] = 4;
+                inventory[M_GATE] = 5;
+                return;
+        }
+    }
+
 }
