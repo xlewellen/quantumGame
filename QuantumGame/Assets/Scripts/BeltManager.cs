b39be0b8ec447717ed6f497d51c83fd9840b8526
pipietrzyk <pipietrzyk0@gmail.com>
2023-03-06 00:23:30 -0600

    idk

--- QuantumGame/Assets/Scripts/BeltManager.cs
+++ QuantumGame/Assets/Scripts/BeltManager.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 
 public class BeltManager : MonoBehaviour
 {
+
+    // TODO: determine direction based on what tile the belt is assigned
+
+    private int RIGHT = 0;
+    private int LEFT = 1;
+    private int UP = 2;
+    private int DOWN = 3;
+
+    // the resource on the belt
+    GameObject resource;
+
+    public int direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = DOWN;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+        switch (direction) {
+            case 0 :
+                resource.transform.Translate(1 * Time.deltaTime ,0,0);
+                break;
+            case 1 :
+                resource.transform.Translate(-1 * Time.deltaTime ,0,0);
+                break;
+            case 2 :
+                resource.transform.Translate(0,1 * Time.deltaTime,0);
+                break;
+            case 3 :
+                resource.transform.Translate(0,-1 * Time.deltaTime,0);
+                break;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        other.gameObject.transform.Translate(0,10 * Time.deltaTime,0);
+        if (other.gameObject.tag == "Object")
+            resource = other.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Object")
+            resource = null;
     }
 }
