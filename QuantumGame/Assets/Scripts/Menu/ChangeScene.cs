1047e5a33e9dadccf01f515ce871f5f375947bf0
xlewellen <xalewellen@gmail.com>
2023-03-09 00:44:44 -0600

    Fixed Pause Bug again

--- QuantumGame/Assets/Scripts/Menu/ChangeScene.cs
+++ QuantumGame/Assets/Scripts/Menu/ChangeScene.cs
@@ -6,7 +6,13 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     public void MoveToScene(string sceneName) {
+        Resume();
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void Resume()
+    {
+        Time.timeScale = 1f;
     }
 
     public void QuitGame()
